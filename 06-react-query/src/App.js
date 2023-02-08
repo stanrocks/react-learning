46058b2776c7f6ee5f653e6d2ee2c10db6830423
stanrocks <25880632+stanrocks@users.noreply.github.com>
2023-02-08 23:43:11 +0600

    react-query init

--- 06-react-query/src/App.js
+++ 06-react-query/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// https://tanstack.com/query/latest/docs/react/overview
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const queryClient = new QueryClient();
+
+	return (
+		<>
+			<QueryClientProvider client={queryClient}>
+				<p>random</p>
+			</QueryClientProvider>
+		</>
+	);
 }
 
 export default App;
