6a5473cac10686b98648876171a9df840afec3dd
stanrocks <25880632+stanrocks@users.noreply.github.com>
2023-02-16 23:50:00 +0600

    feat(react testing library): starter code

---
+++ 09-react-testing-library/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function App() {
+  const [accountBalance, setAccountBalance] = useState(5000);
+  const [transferAmount, setTransferAmount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    setTimeout(() => {
+      setAccountBalance((prev) => prev - transferAmount);
+      setTransferAmount(0);
+    }, 2000);
+  };
+
+  return (
+    <>
+      <h1>Account</h1>
+
+      <p>Current account balance: {accountBalance}</p>
+
+      <p>Send money to: John Doe</p>
+      <form onSubmit={onSubmit}>
+        <br />
+        <input
+          min="0"
+          type="number"
+          value={transferAmount}
+          onChange={(e) => setTransferAmount(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  );
+}
